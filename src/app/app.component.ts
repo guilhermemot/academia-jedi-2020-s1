192361e2de5cb64340620be8ade7202056fc022c
glaydson <glaydsonvs@gmail.com>
2020-03-10 21:45:54 -0300

    Código do exercício 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: '',
+  selector: 'jedi',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'academia-jedi2020-s1';
+  titulo: string = "Academia Jedi";
 }
